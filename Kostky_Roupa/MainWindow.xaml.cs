dd7d65cfbe588ce500591c818e17961697dffc74
Daniel Roupa <roupadaniel058@gmail.com>
2024-06-06 14:52:08 +0200

    pata cisla

--- Kostky_Roupa/MainWindow.xaml.cs
+++ Kostky_Roupa/MainWindow.xaml.cs
@@ -47,7 +47,36 @@ namespace Kostky_Roupa
             Canvas.SetLeft(obryskostky, x);
             platno.Children.Add(obryskostky);
             
-
+            
+        }
+        private void cisla(int x, int y, int hodnota, int vzdalenost)
+        {
+            double cislovelikost = vzdalenost / 5;
+            double odsazeni = vzdalenost / 4;
+            double vycentrovano = vzdalenost / 2;
+            Point[] pozice = new Point[]
+            {
+                new Point(vycentrovano, vycentrovano),
+                new Point(vycentrovano - odsazeni, vycentrovano - odsazeni),
+                new Point(vycentrovano + odsazeni, vycentrovano + vycentrovano),
+                new Point(vycentrovano - odsazeni, vycentrovano + odsazeni),
+                new Point(vycentrovano + odsazeni, vycentrovano - vycentrovano),
+                new Point(vycentrovano - odsazeni, vycentrovano),
+                new Point(vycentrovano + odsazeni, vycentrovano)
+            };
+            Ellipse[] cisla = new Ellipse[hodnota];
+            for (int i = 0; i < hodnota; i++)
+            {
+                cisla[i] = new Ellipse
+                {
+                    Width = cislovelikost,
+                    Height = cislovelikost,
+                    Fill = Brushes.Black
+                };
+                Canvas.SetTop(cisla[i], y + pozice[i].Y - cislovelikost / 2);
+                Canvas.SetLeft(cisla[i], x + pozice[i].X - cislovelikost / 2);
+                platno.Children.Add(cisla[i]);
+            }
         }
         private void vykreslitkostky()
         {

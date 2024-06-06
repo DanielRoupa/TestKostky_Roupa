d390480a2d53df611aa5807aede72da0c8c649ae
Daniel Roupa <roupadaniel058@gmail.com>
2024-06-06 13:55:49 +0200

    Přidejte soubory projektu.

---
+++ Kostky_Roupa/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Kostky_Roupa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private hra Hra;
+        private DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Hra = new hra();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(30);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            platno.Children.Clear();
+            Hra.Draw(platno);
+        }
+
+        private void btnhod_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kostky_Roupa/hra.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Kostky_Roupa
+{
+    public class hra
+    {
+        public List<kostky> GameObjects { get; set; }
+        public hra()
+        {
+            GameObjects = new List<kostky>();
+        }
+        public void Draw(Canvas canvas)
+        {
+            foreach (kostky obj in GameObjects)
+            {
+
+                if (obj != null)
+                {
+
+                    obj.Draw(canvas);
+                }
+            }
+        }
+    }
+}
